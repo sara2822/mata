5d38a624cd4b956e61d484da64e5557f57dd4125
sara2822 <sara2.ferlin@gmail.com>
2015-12-09 20:29:40 +0100

    dodan projekt
    Kar cel projekt sm skopirala gor. Hope this works.

---
+++ Assets/Scripts/gameStart.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameStart : MonoBehaviour {
+	
+	private SpriteRenderer FadeIn;
+	public float FadeInSpeed = 0.9f;
+	private float alpha = 1f;
+	private bool newLevelLoaded = false;
+	private bool loadNewLevel = false;
+	
+	// Use this for initialization
+	void Start () {
+		FadeIn = gameObject.GetComponent <SpriteRenderer>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit2D hit = Physics2D.GetRayIntersection (ray);
+
+		if (hit && loadNewLevel == false) {
+			if (hit.collider.name == "colider" && Input.GetMouseButton (0)) {
+				loadNewLevel = true;
+				Debug.Log (loadNewLevel);
+			}
+		}
+		if (loadNewLevel == true) {
+			alpha -= FadeInSpeed * Time.deltaTime;
+			alpha = Mathf.Clamp01 (alpha);
+				Debug.Log (alpha);
+			
+			FadeIn.color = new Color (FadeIn.color.r, FadeIn.color.g, FadeIn.color.b, alpha);
+			if (alpha <= 0.01f && newLevelLoaded == false) {
+					Application.LoadLevel (Application.loadedLevel + 1);
+					newLevelLoaded = true;
+			}
+		}	
+	}
+}
---
+++ Assets/Scripts/scene1.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scene1 : MonoBehaviour {
+	// original game object tukaj je mama, ona kontrolira celotno sceno
+
+	public GameObject Boy;
+	public GameObject Girl;
+	private int sceneIT; //odvijanje scene 
+	public GameObject choosePlayer;
+	public GameObject insertName;
+
+
+	public bool playerChosen;
+
+	// Use this for initialization
+	void Start () {
+		sceneIT = 0;
+	}
+
+	
+	// Update is called once per frame 
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit2D hit = Physics2D.GetRayIntersection (ray);
+
+		if (sceneIT == 0) {
+			choosePlayer.SetActive(true);
+			if (hit && !playerChosen) {
+				if (hit.collider.name == "Girl"  && Input.GetMouseButton (0)){
+					Boy.SetActive(false);
+					playerChosen = true;
+					sceneIT++;
+					choosePlayer.SetActive(false);
+				}
+				if (hit.collider.name == "Boy"  && Input.GetMouseButton (0)) {
+					Girl.SetActive(false);
+					playerChosen = true;
+					sceneIT++;
+					choosePlayer.SetActive(false);
+
+				}
+			}
+		}
+		if (sceneIT == 1) {
+			insertName.SetActive(true);
+		}
+	
+	}
+}
